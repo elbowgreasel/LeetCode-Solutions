6ec562ddbd7bebe2183b3136d2959d9d0130a3c2
Jaden Kropp <98624709+elbowgreasel@users.noreply.github.com>
2022-10-15 00:07:13 -0600

    Update easy-questions.js

--- easy-questions.js
+++ easy-questions.js
@@ -80,4 +80,22 @@ var removeDuplicates = function(nums) {
     return step+1
 };
 
+// Remove Element
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let kCounter = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== val){
+            nums[kCounter] = nums[i]
+            kCounter++
+        }
+    }
+    return kCounter
+};
+
 
